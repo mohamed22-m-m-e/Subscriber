91861fa353d044d7c476c6be0abfe291b65e5a7c
user <user@DESKTOP-UBH533L>
2025-02-22 22:14:31 +0400

    Add project files.

---
+++ Subscriber.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    public delegate void Notificationhandler (string message);
+    public class Subscriber : Colors
+    {
+        public event Notificationhandler OnNotification;
+        public void Notification (string message)
+        {
+            if (OnNotification != null)
+            {
+                OnNotification (message);
+            }
+            else
+            {
+                RED ();
+                Console.WriteLine ("You Have No Subscribers:(");
+                WHITE ();
+            }
+        }
+    }
+    public class User : Colors
+    {
+        public string Name { get; set; }
+        public List<string> Message = new List<string> ();
+        void Separate ()
+        {
+            Console.WriteLine ("=============== - ================ - ==================");
+        }
+        public void ReceiveNotification (string message)
+        {
+            Message.Add (message);
+            BLUE ();
+            Console.WriteLine ($"Message Sent For {Name} From : {message}.");
+            WHITE ();
+            Separate ();
+        }
+        public void AlreadyExist ()
+        {
+            RED ();
+            Console.WriteLine ("This Username Is Already Taken.");
+            WHITE ();
+            Separate ();
+        }
+        public void NotFound ()
+        {
+            RED ();
+            Console.WriteLine ("Your Username Not Found | Or You May Be Unsubscribed Already.");
+            WHITE ();
+            Separate ();
+        }
+        public void NoSubscribers ()
+        {
+            RED ();
+            Console.WriteLine ("There Are No Subscribers:(");
+            WHITE ();
+            Separate ();
+        }
+        public void Subscribe ()
+        {
+            GREEN ();
+            Console.WriteLine ($"{Name} Subscribed Successfully!");
+            WHITE ();
+        }
+        public void Unsubscribe ()
+        {
+            RED ();
+            Console.WriteLine ($"{Name} Unsubscribed Successfully");
+            WHITE ();
+        }
+        public void SeeNotifications ()
+        {
+            if (Message.Count != 0)
+            {
+                Separate ();
+                BLUE ();
+                Console.WriteLine ("Your Inbox:");
+                WHITE ();
+                for (int i = 0; i < Message.Count; i++)
+                {
+                    Console.WriteLine (Message[i]);
+                }
+                Separate ();
+            }
+            else
+            {
+                Separate ();
+                RED ();
+                Console.WriteLine ("You Have No Notifications :(");
+                WHITE ();
+                Separate ();
+            }
+        }
+    }
+    public class Colors
+    {
+        public void RED ()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        public void GREEN ()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public void BLUE ()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        public void WHITE ()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Subscriber/Program.cs
@@ -0,0 +1,129 @@
+﻿using Subscriber;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.Configuration;
+using System.Reflection.Emit;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Subscriber 
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            Subscriber subscriber = new Subscriber();
+            User user = new User ();
+            List<User> list = new List<User> ();
+            while (true)
+            {
+                Console.WriteLine ("1.Subscribe  -  2.Send Notification   -   3.View Notifications  -   4.Unsubscribe   -   5.Shut Down");
+                Console.Write ("Action: ");
+                int answer = int.Parse (Console.ReadLine ());
+                switch (answer)
+                {
+                    case 1:
+                        Console.Write ("Enter your Username: ");
+                        string user_name = Console.ReadLine ();
+                        int i = 0;
+                        do
+                        {
+                            if (list.Count == 0)
+                            {
+                                user.Name = user_name;
+                                list.Add (user);
+                                subscriber.OnNotification += list[i].ReceiveNotification;
+                                list[i].Subscribe ();
+                                break;
+                            }
+                            else if (list[i].Name.Equals (user_name, StringComparison.OrdinalIgnoreCase) == true)
+                            {
+                                user.AlreadyExist ();
+                                i++;
+                                break;
+                            }
+                            else if (list.Count - 1 == i)
+                            {
+                                list.Add (new User () { Name = user_name });
+                                subscriber.OnNotification += list[i += 1].ReceiveNotification;
+                                list[i].Subscribe ();
+                                break;
+                            }
+                            i++;
+                        } while (i < list.Count);
+                        break;
+                    case 2:
+                        Console.Write ("Send The Message: ");
+                        string message = Console.ReadLine ();
+                        subscriber.Notification (message);
+                        break;
+                    case 3:
+                        if (list.Count != 0)
+                        {
+                            Console.Write ("Enter Your Username: ");
+                            string Name = Console.ReadLine ();
+                            for (i = 0; i < list.Count; i++)
+                            {
+                                if (list[i].Name.Equals (Name, StringComparison.OrdinalIgnoreCase) == true)
+                                {
+                                    list[i].SeeNotifications ();
+                                    break;
+                                }
+                                else if (list.Count - 1 == i)
+                                {
+                                    user.RED ();
+                                    Console.WriteLine ("Your Username Not Found");
+                                    user.WHITE ();
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            user.NoSubscribers ();
+                            break;
+                        }
+                    case 4:
+                        if (list.Count != 0)
+                        {
+                            Console.Write ("Please Enter Your Username: ");
+                            string name = Console.ReadLine ();
+                            {
+                                for (i = 0; i < list.Count; i++)
+                                {
+                                    if (list[i].Name.Equals (name, StringComparison.OrdinalIgnoreCase) == true)
+                                    {
+                                        list[i].Unsubscribe ();
+                                        subscriber.OnNotification -= list[i].ReceiveNotification;
+                                        break;
+                                    }
+                                    else if (list.Count - 1 == i)
+                                    {
+                                        user.NotFound ();
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            user.NoSubscribers ();
+                            break;
+                        }
+                    case 5:
+                        user.RED ();
+                        Console.WriteLine ("Shutting Down...");
+                        user.WHITE ();
+                        return;
+                }
+            }
+        }
+    }
+}
